bdfdefec0d382064130409e0d9b43960c87e1b01
Roman Yankowski <romanjankowski@groupoffice.ch>
2021-09-01 17:35:15 +0200

    Place Content Component Update

--- src/Layout/PlaceContent/index.stories.tsx
+++ src/Layout/PlaceContent/index.stories.tsx
@@ -1,16 +1,64 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import PlaceContent from "./";
+import Container from "../Container"
 
 export default {
   title: 'Layout/PlaceContent',
   component: PlaceContent
 } as ComponentMeta<typeof PlaceContent>;
 
-const Template: ComponentStory<typeof PlaceContent> = (args) => <PlaceContent {...args} />;
+const children = <div>Text</div>
 
-export const Template_Example = Template.bind({});
-Template_Example.args = {
-  foo: "bar"
+const Template: ComponentStory<typeof PlaceContent> = (args) => {
+  return <Container padding="large" style={{minHeight: 300}}><PlaceContent {...args} style={{ minHeight: "inherit"}}/></Container>
+}
+
+export const TopLeft = Template.bind({});
+TopLeft.args = {
+  children,
+  verticalDirection: "top",
+  horizontalDirection: "left"
 };
 
+export const TopRight = Template.bind({});
+TopRight.args = {
+  children,
+  verticalDirection: "top",
+  horizontalDirection: "right"
+};
+
+export const TopCenter = Template.bind({});
+TopCenter.args = {
+  children,
+  verticalDirection: "top",
+  horizontalDirection: "center"
+};
+
+export const CenterCenter = Template.bind({});
+CenterCenter.args = {
+  children,
+  verticalDirection: "center",
+  horizontalDirection: "center"
+};
+
+export const BottonLeft = Template.bind({});
+BottonLeft.args = {
+  children,
+  verticalDirection: "bottom",
+  horizontalDirection: "left"
+};
+
+export const BottonRight = Template.bind({});
+BottonRight.args = {
+  children,
+  verticalDirection: "bottom",
+  horizontalDirection: "right"
+};
+
+export const BottonCenter = Template.bind({});
+BottonCenter.args = {
+  children,
+  verticalDirection: "bottom",
+  horizontalDirection: "center"
+};
