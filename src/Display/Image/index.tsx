b5007f50c1123adbe58cf2672f2ef2fd73f09779
Roman Yankowski <romanjankowski@groupoffice.ch>
2021-09-09 16:40:49 +0200

    upd

--- src/Display/Image/index.tsx
+++ src/Display/Image/index.tsx
@@ -2,6 +2,27 @@ import React, { useEffect, useState } from "react";
 import { useIPFS } from "react-ipfs";
 import ImageProps from "./index.types";
 
+export function getMime(extension: string) {
+  return (
+    ({
+      "svg": "image/svg+xml",
+      "webp": "image/webp",
+      "png": "image/png",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "jfif": "image/jpeg",
+      "pjpeg": "image/jpeg",
+      "pjp": "image/jpeg",
+      "gif": "image/gif",
+      "avif": "image/avif",
+      "apng": "image/apng"
+    } as any)[extension] || "")
+}
+
+export function getExtension(url: string) {
+  return url.slice((url.lastIndexOf(".") - 1 >>> 0) + 2)
+}
+
 const Image: React.FC<ImageProps> = ({ className, style, alt, src }) => {
   const { ipfs, isIpfsReady } = useIPFS()
   const [imgSrc, setImgSrc] = useState<string>("")
@@ -11,8 +32,13 @@ const Image: React.FC<ImageProps> = ({ className, style, alt, src }) => {
     if (isIpfsReady && ipfs) {
       if (src && src.includes("ipfs://")) {
         ipfs.read(src.slice(7)).then(([file]) => {
-          setImgSrc(file?.toObjectURL() || "")
+          if (file) {
+            const fileData = file.toFile()
+            setImgSrc(URL.createObjectURL(fileData));
+          }
         })
+      } else {
+        setImgSrc(src)
       }
     } else {
       if (src && !src.includes("ipfs://")) {
@@ -32,7 +58,10 @@ export const loadImage = (src: string) => {
     if (isIpfsReady && ipfs) {
       if (src && src.includes("ipfs://")) {
         ipfs.read(src.slice(7)).then(([file]) => {
-          setImgSrc(file?.toObjectURL() || "")
+          if (file) {
+            const fileData = file.toFile()
+            setImgSrc(URL.createObjectURL(fileData));
+          }
         })
       }
     } else {
