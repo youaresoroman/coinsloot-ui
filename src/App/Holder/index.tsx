d24440498040c954a83fafb893ebc3cea1a13ac8
Roman Yankowski <romanjankowski@groupoffice.ch>
2021-09-14 12:07:23 +0200

    upd

--- src/App/Holder/index.tsx
+++ src/App/Holder/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 import AppProps from "../index.types";
 import { startIPFSInstance } from "react-ipfs";
-import { useEffect } from "react";
 import useTheme from "../../Hooks/useTheme";
-import { useNotifications } from "../../Feedback/Toast";
 import withNotifications from "../../Hooks/withNotifications";
 
 const App: React.FC<AppProps> = ({ children }) => {
